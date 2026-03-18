c07a64bedd0c767cf35a35075eb25fc9d457d182
Yodes <35444748+Yobas@users.noreply.github.com>
2026-03-18 21:43:01 +0300

    remove test files

--- Src/RVEngine/src/plugins/js_host/extension_src/src/debugAdapter.ts
+++
@@ -1,7 +0,0 @@
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-
- import { QuickJSDebugSession } from './quickjsDebug';
-
- QuickJSDebugSession.run(QuickJSDebugSession);
