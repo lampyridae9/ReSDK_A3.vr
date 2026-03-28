acaf6d0a29c13c418ea499c32e0ae3b58adf2087
Yodes <35444748+Yobas@users.noreply.github.com>
2026-03-28 11:04:11 +0300

    revoicer 1.6.0.0 sha1 update

--- Src/client/ClientInit/_allowed_extensions.h
+++ Src/client/ClientInit/_allowed_extensions.h
@@ -6,12 +6,12 @@
 #include <..\..\host\lang.hpp>
 
 macro_const(clientside_list_allowedExtensions)
-// Generated from systools::generateLibInfo (ReEditor 1.19)
+// Generated from make_sign.py
 #define CLIENTSIDE_LIST_ALLOWED_EXTENSIONS [ \
 	["debug_console", "C0AD30DFFBB32CD85BC73FE39021FD61E2CDE7D1"], \
 	["url_fetch", "54A1D7879C726D6EE9AF99D0AABEA6CE83765003"], \
 	["DiscordRichPresence", "98ADB813FF702F49743E830CE1C6A3A6FBEFFBD0"], \
 	["task_force_radio_pipe", "35F2CFED92ADBFE763F5C8FDD48764286F159AF5"], \
-	["revoicer", "EA9F84AA0573A8E8DED4DE9DB4348D993D1A6444"], \
+	["revoicer", "BDD631810DF4F1201A8B30DEE9FF422D442EB8E9"], \
 	["real_date", "179A73FD58174CA7B45B052F1777161D641163A9"] \
  ]
